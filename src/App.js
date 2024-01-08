83ff544fe2e14db2af72c9dff791e7b9fa52bfb3
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-08 11:03:07 -0800

    used props to update ColorBlock component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Color from './components/color';
 import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
 
 function App() {
     let colors = ['violet', 'blue', 'lightblue', 'green'];
-    return (
-        <div className="App">
-            <Color />
-        </div>
-    );
+    let colorMap = colors.map((color, i) => {
+        return <ColorBlock color={color} />;
+    });
+    return <div className="App">{colorMap}</div>;
 }
 
 export default App;
