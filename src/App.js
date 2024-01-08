d2b62452dcd3be9ef05ed3f946bab70687ed8d5d
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-08 12:05:04 -0800

    created addColor variable that passes newColor as an argument to our state Array of colors using spread operator
    defined handleSubmit helper function and passed newColor down to child component as props to finalize submit button functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Color from './components/color';
 import React, { useState } from 'react';
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm.js';
 
 function App() {
+    const addColor = (newColor) => {
+        setColors([...colors, newColor]);
+    };
     let [colors, setColors] = useState(['red', 'orange', 'yellow', 'green', 'lightblue', 'blue', 'violet']);
     let colorMap = colors.map((color, i) => {
         return <ColorBlock key={i} color={color} />;
     });
-    return <div className="App">{colorMap}</div>;
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    );
 }
 
 export default App;
--- src/ColorForm.js
+++ src/ColorForm.js
@@ -3,10 +3,16 @@ import React, { useState } from 'react';
 export default function ColorForm(props) {
     let [input, setInput] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addColor(input);
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Submit!</button>
             </form>
         </div>
     );
